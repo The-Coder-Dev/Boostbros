ef306166c803067ae91a632d41e8cda6a8799952
Dev Sharma <devsharma2057@gmail.com>
2025-12-14 23:34:16 +0530

    NavBar Updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi";
+import { FiMenu, FiX, FiInstagram, FiFacebook } from "react-icons/fi";
 import { motion, AnimatePresence } from "framer-motion";
 
 const links = [
@@ -15,7 +15,10 @@ const Navbar = () => {
   return (
     <header className="w-full flex items-center justify-center h-20  absolute top-0 left-0 z-999 text-white">
       <nav className="mx-auto max-w-375 flex items-center justify-between w-full h-full px-5">
-        <Link href="/" className="uppercase font-bold tracking-wide mix-blend-difference">
+        <Link
+          href="/"
+          className="uppercase font-bold tracking-wide mix-blend-difference"
+        >
           BoostBros
         </Link>
 
@@ -44,8 +47,11 @@ const Navbar = () => {
               );
             })}
           </div>
-          
-          <Link href="/" className="bg-secondary hover:bg-[#fc5a3a] transition-colors duration-300 hidden md:flex rounded-full px-5 py-2 ring-2 ring-[#fc5a3a] text-sm">
+
+          <Link
+            href="/"
+            className="bg-secondary hover:bg-[#fc5a3a] transition-colors duration-300 hidden md:flex rounded-full px-5 py-2 ring-2 ring-[#fc5a3a] text-sm"
+          >
             <p>Let's Talk</p>
           </Link>
         </div>
@@ -90,14 +96,14 @@ const Navbar = () => {
                       className="group overflow-hidden relative text-5xl font-black hover:text-orange transition-colors duration-300"
                     >
                       <Link
-                        className="font-[inter]  uppercase block transition-transform duration-300 group-hover:-translate-y-4"
+                        className="font-[inter]  uppercase block transition-transform duration-300 group-hover:-translate-y-14"
                         href={item.url}
                       >
                         {item.title}
                       </Link>
 
                       <Link
-                        className="absolute left-0 top-16 block transition-transform duration-300 group-hover:-translate-y-16 font-[inter] uppercase"
+                        className="absolute left-0 top-16 block transition-all duration-300 group-hover:-translate-y-16 group-hover:text-secondary  font-[inter] uppercase"
                         href={item.url}
                       >
                         {item.title}
@@ -107,8 +113,23 @@ const Navbar = () => {
                 })}
               </div>
 
-              <div className="w-full bg-secondary absolute left-0 right-0 bottom-0">
-                <p>devstudio@gmail.com</p>
+              <div className="w-full flex flex-col gap-2 items-center justify-center py-3 text-lg bg-secondary absolute left-0 right-0 bottom-0">
+                <Link href="mailto:devstudio2057@gmail.com">
+                  devstudio2057@gmail.com
+                </Link>
+                <div className="flex items-center justify-center w-full gap-2">
+                  <p className="">Follow Us:</p>
+                  <div className="flex gap-2">
+                    <Link href="mailto:devstudio2057@gmail.com" className="border  p-2 rounded-full">
+                      <FiInstagram />
+                    </Link>
+
+                    <Link href="mailto:devstudio2057@gmail.com" className="border  p-2 rounded-full">
+                      <FiFacebook  />
+                    </Link>
+
+                  </div>
+                </div>
               </div>
             </div>
           </motion.div>
